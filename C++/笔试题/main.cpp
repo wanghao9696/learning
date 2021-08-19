bffff98e000a7f5e55b29da484d991550632ac76
wanghao <wanghaobjyd@163.com>
2021-08-19 14:11:49 +0800

    update

--- C++/笔试题/main.cpp
+++ C++/笔试题/main.cpp
@@ -1,57 +1,80 @@
 #include<iostream>
 #include<string>
+#include<sstream>
 #include<vector>
-#include<map>
 #include<algorithm>
 
 using namespace std;
 
-int main()
-{
+int main(){
     string s;
-    cin >> s;
+    vector<string> vec;
+    while(cin >> s){
 
-    map<char, int> m;
-
-    for(int i=0; i<s.length(); i++)
-    {
-        if(m.count(s[i]) == 0)
-        {
-            m.insert(pair<char, int>(s[i], 1));
+        stringstream ss(s);
+        string item;
+        while(getline(ss,item,',')){
+            vec.push_back(item);
         }
-        else
+        sort(vec.begin(),vec.end());
+        reverse(vec.begin(), vec.end());
+
+        int a_1 = atoi(vec[4].c_str());
+        int a_2 = atoi(vec[3].c_str());
+
+        if(a_1 != 0)
         {
-            m[s[i]]++;
+            vector<int> cha;
+            for(int i=0; i<4; i++)
+            {
+                cha.push_back(atoi(vec[i].c_str()) - atoi(vec[i+1].c_str()));
+            }
+            if(count(cha.begin(),cha.end(),1) == 4)
+            {
+                cout << "So Lucky!" << endl;
+            }
+            else
+            {
+                cout << "Oh My God!" << endl;
+            }
+        }
+
+        if(a_1 == 0 && a_2 != 0)
+        {
+            vector<int> cha;
+            for(int i=0; i<3; i++)
+            {
+                cha.push_back(atoi(vec[i].c_str()) - atoi(vec[i+1].c_str()));
+            }
+            if(count(cha.begin(),cha.end(),1) == 2 && count(cha.begin(),cha.end(),2) == 1)
+            {
+                cout << "So Lucky!" << endl;
+            }
+            else
+            {
+                cout << "Oh My God!" << endl;
+            }
+        }
+
+        if(a_1 == 0 && a_2 == 0)
+        {
+            vector<int> cha;
+            for(int i=0; i<2; i++)
+            {
+                cha.push_back(atoi(vec[i].c_str()) - atoi(vec[i+1].c_str()));
+            }
+            cout <<  cha[0] << endl;
+            cout <<  cha[1] << endl;
+            if((cha[0] == 1 || cha[0] == 2) && (cha[1] == 1 || cha[1] == 2))
+            {
+                cout << "So Lucky!" << endl;
+            }
+            else
+            {
+                cout << "Oh My God!" << endl;
+            }
         }
     }
-
-    vector<int> v;
-    for(map<char, int>::iterator it = m.begin(); it!=m.end(); it++)
-    {
-        v.push_back(it->second);
-    }
-    sort(v.begin(), v.end());
-
-
-    int num = v.front();
-    vector<char> c;
-    for(map<char, int>::iterator it = m.begin(); it!=m.end(); it++)
-    {
-        if(it->second == num)
-            c.push_back(it->first);
-    }
-
-
-    for(string::iterator it = s.begin(); it != s.end(); it++)
-    {
-        if(count(c.begin(), c.end(), *it) > 0)
-        {
-            s.erase(it);
-            it--;
-        }
-    }
-
-    cout << s << endl;
-    
+      
     return 0;
 }
--- Mysql/mysql基础.sql
+++ Mysql/mysql基础.sql
@@ -1,6 +1,9 @@
+-- mysql安装成功后，默认root用户密码为空，使用以下命令创建root用户的密码
+mysqladmin -u root passward "hao123123"
 
 -- 登陆
-mysql -uroot -phao123123
+mysql -u root -p
+password:hao123123
 
 -- 查询数据库服务器中的所有数据库
 show databases;
@@ -37,3 +40,24 @@ delete from pet where name='Puffball';
 
 -- 修改数据
 update pet set name='旺财' where owner='Duane';
+
+1399421607
+
+-- 修改字段
+alter table tb1 drop i; -- 删除
+alter table tb1 add i int; -- 添加
+alter table tb1 add i int first; -- 添加到第一列
+alter table tb1 add i int after j; -- 添加到指定位置 
+alter table tb1 modify c char(10); -- 调整c的类型
+alter table tb1 change c i int; -- 调整c的名称和类型
+
+-- 修改表明
+alter table tb1 rename to tb2;
+
+
+
+-- 导出导入文件权限设置
+show variables like '%secure%'; -- 查看权限是否为NULL
+参考：https://blog.csdn.net/qq_20417499/article/details/84621838?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-1.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromMachineLearnPai2%7Edefault-1.control
+[mysqld]
+secure_file_priv = 
